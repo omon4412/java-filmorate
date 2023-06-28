83e92fd3690f9a74a2415e29df7ca438308ef05e
Vetrov Sergey <vetrov241201@yandex.ru>
2023-06-28 22:06:37 +0300

    Убрал неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,9 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FilmorateApplication {
